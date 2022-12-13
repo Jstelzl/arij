a5227006c2032326dc4f1f1e0113a2ad090593bb
[josiah stelzl] <josiahstelzl@gmail.com>
2022-12-13 14:38:20 -0500

    worked on nav, PageContainer, and App.js

---
+++ client/src/components/PageContainer/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Navbar from "../nav";
+
+export default function PageContainer() {
+  const [currentPage, setCurrentPage] = useState("About Me");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "About Me") return <Navbar />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
+
+
+
+
