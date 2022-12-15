98e15fa5232694ad7872798b68001fe086ee6237
[josiah stelzl] <josiahstelzl@gmail.com>
2022-12-14 21:02:20 -0500

    worked on login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,24 +3,7 @@ import "../../App.css";
 
 function Login() {
 
-  return (
-    // <form id="login-form">
-    //   <div className="form-inner">
-    //     <h2>Login</h2>
-    //     <div className="form-group">
-    //       <label htmlFor="name">Name:</label>
-    //       <input type="text" name="name" id="name" />
-    //     </div>
-    //     <div className="form-group">
-    //       <label htmlFor="name">Password:</label>
-    //       <input type="text" name="password" id="password" />
-    //     </div>
-    //     <div className=''>
-    //       <button data-testid="button" type="login">Login</button>
-    //     </div>
-    //   </div>
-    // </form>
-
+  return(
     <div className="max-w-md content-center sm:mx-auto">
       <card>
         <form className="flex flex-col gap-4 content-center sm:mx-auto">
