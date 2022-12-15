0de92b95e029fa7b2da599575c6a719ded1b0d67
[josiah stelzl] <josiahstelzl@gmail.com>
2022-12-15 12:37:49 -0500

    deleted old commented out code

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -4,53 +4,6 @@ import "../../App.css";
 function Login() {
 
   return (
-    // <div className="max-w-md content-center sm:mx-auto min-h-screen">
-    //   <card>
-    //     <form className="flex flex-col gap-4 content-center sm:mx-auto">
-    //       <div>
-    //         <div className="mb-2 block">
-    //           <label
-    //             htmlFor="name"
-    //             value="Your Name"
-    //           />
-    //         </div>
-    //         <input
-    //           id="name"
-    //           type="name"
-    //           placeholder="User Name"
-    //           required={true}
-    //           className="content-center sm:mx-auto block w-full p-2 pl-10 text-sm text-gray-900 border border-blue-900 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    //         />
-    //       </div>
-    //       <div>
-    //         <div className="mb-2 block">
-    //           <label
-    //             htmlFor="password1"
-    //             value="Your Password"
-    //           />
-    //         </div>
-    //         <input
-    //           id="password1"
-    //           type="password"
-    //           placeholder="Password"
-    //           required={true}
-    //           className="block w-full p-2 pl-10 text-sm text-gray-900 border border-blue-900 rounded-lg bg-gray-50 
-    //           focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
-    //           dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    //         />
-    //       </div>
-    //       <div className="sm:mx-auto">
-    //         <button
-    //           className="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-    //           outline={true}
-    //         >
-    //           Login
-    //         </button>
-    //       </div>
-    //     </form>
-    //   </card>
-    // </div>
-
     <form className="w-full max-w-xl max-w-lg content-center sm:mx-auto min-h-screen m-10 p-5 login-border-color border border-4">
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
