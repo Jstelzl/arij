8ea83a36cbe8d3b4a8410e1ed4f08179882f2ea8
[josiah stelzl] <josiahstelzl@gmail.com>
2022-12-16 12:22:07 -0500

    worked on login

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -8,7 +8,7 @@ function Login() {
       <ul className="flex flex-wrap text-sm font-medium text-center text-blue-900 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" 
       id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
         <li className="mr-2">
-          <button id="login-tab" data-tabs-target="#login" type="button" role="tab" aria-controls="login" aria-selected="true" className="inline-block p-4 text-blue-900 rounded-tl-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500">Login</button>
+          <button id="login-tab" data-tabs-target="#login" type="button" role="tab" aria-controls="login" aria-selected="true" className="inline-block p-4 text-blue-900 rounded-tl-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500">Logi</button>
         </li>
         <li className="mr-2">
           <button id="signup-tab" data-tabs-target="#signup" type="button" role="tab" aria-controls="signup" aria-selected="false" className="inline-block p-4 hover:text-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300">Sign Up</button>
