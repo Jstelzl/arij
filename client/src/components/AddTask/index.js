35352184c7d75afd2d10182703e05e32d9594318
Alyssa Petersen <aptrsn11@gmail.com>
2022-12-17 11:25:52 -0500

    implement date picker

--- client/src/components/AddTask/index.js
+++ client/src/components/AddTask/index.js
@@ -1,43 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Datepicker from "flowbite-datepicker/Datepicker";
 
 function Modal({ setShowModal, columnTitle, addItem }) {
   const [task, setTask] = useState("");
 
+  useEffect(() => {
+    const datepickerEl = document?.getElementById("datepickerId");
+    // console.log(datepickerEl);
+    new Datepicker(datepickerEl, {});
+  }, []);
+
   return (
- <>
-        <div className='modal'>
-        <div class="relative w-full h-full max-w-md md:h-auto">
+    <>
+      <div className="modal">
+        <div className="relative w-full h-full max-w-md md:h-auto">
           {/* <!-- Modal content --> */}
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button
-              type="button"
-              class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-              data-modal-toggle="authentication-modal"
-            >
-              <svg
-                aria-hidden="true"
-                class="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span class="sr-only">Close modal</span>
-            </button>
-            <div class="px-6 py-6 lg:px-8">
-              <h3 class="mb-4 text-xl text-center font-medium text-gray-900 dark:text-white">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="px-6 py-6 lg:px-8" id="ticket-modal">
+              <h3 className="mb-4 text-xl text-center font-medium text-gray-900 dark:text-white">
                 Add a Task
               </h3>
-              <form class="space-y-6" action="#">
+              <form className="space-y-6" action="#">
                 <div>
                   <label
                     for="ticketTitle"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Task Title:
                   </label>
@@ -45,37 +32,35 @@ function Modal({ setShowModal, columnTitle, addItem }) {
                     type="text"
                     name="ticketTitle"
                     id="ticketTitle"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    placeholder="name@company.com"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                    placeholder="Enter a title here"
                     required
                   />
                 </div>
                 <div>
                   <label
-                    for="large-input"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    for="ticketBody"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Describe the task:
+                    Please describe the task:
                   </label>
-                  <input
-                    type="text"
-                    name="ticketBody"
-                    id="large-input"
-                    placeholder="please enter a task description..."
-                    class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required
-                  />
+                  <textarea
+                    id="message"
+                    rows="4"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter a description here..."
+                  ></textarea>
                 </div>
                 <div>
                   <label
                     for="urgencyLevel"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Select an urgency level for the task:
                   </label>
                   <select
                     id="urgencyLevel"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   >
                     <option selected>Choose an urgency</option>
                     <option value="Low">Low</option>
@@ -83,18 +68,26 @@ function Modal({ setShowModal, columnTitle, addItem }) {
                     <option value="High">High</option>
                   </select>
                 </div>
-
-                <div class="relative">
-                <label
-                    for="dueBy"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Select a due date for the task:
-                  </label>
-                  <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <div className="w-full">
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Pick a due date for the task:</label>
+                <div className="relative w-90 items-center">
+                  <input
+                    datepicker
+                    datepicker-autohide
+                    type="text"
+                    className= "items-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="Select due date"
+                    onSelect={(e) => console.log(e.target.value)}
+                    // onClick={(e) => dobHandler(e)}
+                    // onClick={(e) => console.log(e.target.value)}
+                    // onChange={(e) => console.log(e)}
+                    id="datepickerId"
+                  />
+                  <div className="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
                     <svg
                       aria-hidden="true"
-                      class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      className="w-5 h-5 text-gray-500 dark:text-gray-400"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
@@ -106,19 +99,14 @@ function Modal({ setShowModal, columnTitle, addItem }) {
                       ></path>
                     </svg>
                   </div>
-                  <input
-                    datepicker
-                    type="text"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Select due date"
-                  />
+                </div>
                 </div>
               </form>
             </div>
           </div>
         </div>
-     </div>
- </> 
+      </div>
+    </>
   );
 }
 
--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -6,5 +6,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+]
 }
+
+
