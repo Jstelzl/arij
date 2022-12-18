f10ca1dea02c05b2bb8588410e50a3eb7dee0d0d
[josiah stelzl] <josiahstelzl@gmail.com>
2022-12-18 14:12:58 -0500

    made username input longer, deleted last name input, changed titles

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -31,18 +31,12 @@ function Login() {
         {/* Login Form Tab */}
         <form className=" max-w-md mx-auto my-auto " id="login" role="tabpanel" aria-labelledby="login-tab">
           <div className="flex flex-wrap -mx-3 mb-6 my-12">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <div className="w-full px-3 mb-6 md:mb-0">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                First Name
+                User Name
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Santa" />
               <p className="text-red-500 text-xs italic">Please fill out this field.</p>
-            </div>
-            <div className="w-full md:w-1/2 px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                Last Name
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Claus" />
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
@@ -66,18 +60,12 @@ function Login() {
         : 
         <form className="max-w-md mx-auto my-auto " role="tabpanel" id="signup" aria-labelledby="signup-tab">
           <div className="flex flex-wrap -mx-3 mb-6 my-12">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <div className="w-full px-3 mb-6 md:mb-0">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                First Name
+                User Name
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Santa" />
               <p className="text-red-500 text-xs italic">Please fill out this field.</p>
-            </div>
-            <div className="w-full md:w-1/2 px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                Last Name
-              </label>
-              <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Claus" />
             </div>
           </div>
           <div className="flex flex-wrap -mx-3 mb-6">
@@ -98,14 +86,6 @@ function Login() {
             </button>
           </div>
         </form>
-        
-        {/* Login Form Tab */}
-        
-        {/* End Login Form Tab */}
-
-        {/* SignUp form Tab */}
-        
-        {/* End SignUp form Tab */}
       </div>
     </div>
 
