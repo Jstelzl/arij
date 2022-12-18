f45ce0b940dd58bc860610ca589d800063f559bd
Reece Devenney <reecedevenney@gmail.com>
2022-12-18 17:10:01 -0500

    logout button now logs user out

--- client/src/components/NavLink/index.js
+++ client/src/components/NavLink/index.js
@@ -74,14 +74,14 @@ function NavTabs({ currentPage, handlePageChange }) {
         </li>
         <li>
           {loggedIn ?
-          <a 
+          <button 
           href="#LogOut" 
-          onClick={() => handlePageChange("Log Out")}
+          onClick={() => Auth.logout()}
           className={
             currentPage === "Log Out" ? "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-800 md:p-0 dark:text-white" : "block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-slate-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
           }
         >
-            Log Out</a>
+            Log Out</button>
             :
             <span></span>
           }
