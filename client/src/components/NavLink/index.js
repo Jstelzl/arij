d99bd9fd22d645d06765aa97e62fed90c7b3915f
[josiah stelzl] <josiahstelzl@gmail.com>
2022-12-20 12:59:28 -0500

    fixed spacing

--- client/src/components/NavLink/index.js
+++ client/src/components/NavLink/index.js
@@ -247,69 +247,69 @@ function NavTabs() {
               )}
             </li>
           </ul>
-          
-<button type="button" data-modal-toggle="crypto-modal" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700">
-  Connect wallet
-</button>
 
-{/*  Main modal */}
-<div id="crypto-modal" tabindex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-    <div className="relative w-full h-full max-w-md md:h-auto">
-        {/* <!-- Modal content --> */}
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="crypto-modal">
-                <span className="sr-only">Close modal</span>
-            </button>
-            {/* <!-- Modal header --> */}
-            <div className="px-6 py-4 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-base font-semibold text-gray-900 lg:text-xl dark:text-white">
+          <button type="button" data-modal-toggle="crypto-modal" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700">
+            Connect wallet
+          </button>
+
+          {/*  Main modal */}
+          <div id="crypto-modal" tabindex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+            <div className="relative w-full h-full max-w-md md:h-auto">
+              {/* <!-- Modal content --> */}
+              <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="crypto-modal">
+                  <span className="sr-only">Close modal</span>
+                </button>
+                {/* <!-- Modal header --> */}
+                <div className="px-6 py-4 border-b rounded-t dark:border-gray-600">
+                  <h3 className="text-base font-semibold text-gray-900 lg:text-xl dark:text-white">
                     Connect wallet
-                </h3>
-            </div>
-            {/* <!-- Modal body --> */}
-            <div className="p-6">
-                <p className="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers or create a new one.</p>
-                <ul className="my-4 space-y-3">
-                    <li>
-                        <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-                            <span className="flex-1 ml-3 whitespace-nowrap">MetaMask</span>
-                            <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">Popular</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-                            
-                            <span className="flex-1 ml-3 whitespace-nowrap">Coinbase Wallet</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-                            
-                            <span className="flex-1 ml-3 whitespace-nowrap">Opera Wallet</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-
-                            <span className="flex-1 ml-3 whitespace-nowrap">WalletConnect</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-                            
-                            <span className="flex-1 ml-3 whitespace-nowrap">Fortmatic</span>
-                        </a>
-                    </li>
-                </ul>
-                <div>
-                    <a href="#" className="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
-                        
-                        Why do I need to connect with my wallet?</a>
+                  </h3>
                 </div>
+                {/* <!-- Modal body --> */}
+                <div className="p-6">
+                  <p className="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers or create a new one.</p>
+                  <ul className="my-4 space-y-3">
+                    <li>
+                      <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                        <span className="flex-1 ml-3 whitespace-nowrap">MetaMask</span>
+                        <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">Popular</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+
+                        <span className="flex-1 ml-3 whitespace-nowrap">Coinbase Wallet</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+
+                        <span className="flex-1 ml-3 whitespace-nowrap">Opera Wallet</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+
+                        <span className="flex-1 ml-3 whitespace-nowrap">WalletConnect</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+
+                        <span className="flex-1 ml-3 whitespace-nowrap">Fortmatic</span>
+                      </a>
+                    </li>
+                  </ul>
+                  <div>
+                    <a href="#" className="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
+
+                      Why do I need to connect with my wallet?</a>
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    </div>
-</div>
+          </div>
 
         </div>
       </div>
