cfa34424b68b6120523df74e98f0d6bf2bf48989
[josiah stelzl] <josiahstelzl@gmail.com>
2022-12-21 11:02:05 -0500

    added contact component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,10 +8,7 @@ function Footer() {
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-          <a href="#" className="hover:underline">Contact</a>
+          <a href="#" className="hover:underline">Contact Us</a>
         </li>
       </ul>
     </footer>
