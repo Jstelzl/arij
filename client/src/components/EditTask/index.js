c0cd1e19c80894f25f7c8b3c6cdb0bebda809bc5
Alyssa Petersen <aptrsn11@gmail.com>
2022-12-21 11:19:36 -0500

    fix camel casing in styling elements

--- client/src/components/EditTask/index.js
+++ client/src/components/EditTask/index.js
@@ -130,9 +130,9 @@ function Modal({ setShowModal, columnTitle, editItem, test, activeTask }) {
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                          clip-rule="evenodd"
+                          clipRule="evenodd"
                         ></path>
                       </svg>
                     </div>
